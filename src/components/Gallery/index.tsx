be49e2755967dca704e618401aead92d83e7f6cd
Carlos Silva <carlosemilio.contato@gmail.com>
2024-05-29 05:23:12 -0300

    fix: security

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -3,16 +3,24 @@ import Card from "../Card";
 import Dialog from "../Dialog";
 
 export default function Gallery(){
-  const [openDialog, setOpenDialog] = useState(false)
+  const [showDialog, setShowDialog] = useState(false)
   const [selectedCard, setSelectedCard] = useState<number | undefined>(undefined)
+
+  const closeDialog = () => {
+    setShowDialog(false)
+  }
+
+  const handleClickCard = () => {
+    setShowDialog(true)
+  }
 
   return(
     <section className="w-full flex flex-row flex-wrap justify-center items-center gap-4 p-4">
       {Array.from({ length: 9 }).map((_, i) =>
-          <Card handleClick={() => setOpenDialog(true)} position={i + 1} onSelectNumber={setSelectedCard} />
+          <Card handleClick={handleClickCard} position={i + 1} onSelectNumber={setSelectedCard} />
       )}
 
-      <Dialog isOpen={openDialog} onClose={() => setOpenDialog(false)}> 
+      <Dialog isOpen={showDialog} onClose={closeDialog}> 
         <h2> { selectedCard } </h2>
       </Dialog>
     </section>
